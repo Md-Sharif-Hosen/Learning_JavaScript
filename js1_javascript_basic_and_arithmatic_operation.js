47835e47219f43404057562c80dc42fc65ba9790
Md-Sharif-Hosen <mdsharifkhan762@gmail.com>
2023-05-15 17:18:43 +0600

    javascript basic and arithmetic operation

---
+++ js1_javascript_basic_and_arithmatic_operation.js
@@ -0,0 +1,21 @@
+let a;
+let b;
+let sum_result;
+let sub_result;
+let mul_result;
+let div_result;
+let mod_result;
+
+a=10;
+b=25;
+sum_result=a+b;
+sub_result=a-b;
+mul_result=a*b;
+div_result=b/a;
+mod_result=b%a;
+
+console.log("sumation:" + sum_result);
+console.log("subtraction:" + sub_result);
+console.log("multipication:" + mul_result);
+console.log("division :" + div_result);
+console.log("modulus:" + mod_result);
